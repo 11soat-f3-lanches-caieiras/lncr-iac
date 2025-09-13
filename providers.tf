f10c308359a3e695a4c03f5a37ba747cd634589b
gusta_silva <gustavosilva18052005@gmail.com>
2025-09-13 11:32:53 -0300

    fix: aws modules api, lambda version

--- providers.tf
+++ providers.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "lncr-infra-tfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
