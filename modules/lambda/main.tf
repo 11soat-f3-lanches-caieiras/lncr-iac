700fc6548fa8f834ca651fbaeb2880b8ffe321f8
gusta_silva <gustavosilva18052005@gmail.com>
2025-09-17 21:48:53 -0300

    fix: eks config

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -5,7 +5,7 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
   output_path = "${path.module}/lambda.zip"
-  source_file = "${path.module}/template/empty.txt"
+  source_file = "${path.module}/templates/empty.txt"
 }
 
 resource "aws_lambda_function" "lambda" {
