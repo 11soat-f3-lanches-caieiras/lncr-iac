7c115a9cd9f20af6c6aba164946b604c2313a07e
gusta_silva <gustavosilva18052005@gmail.com>
2025-09-23 20:30:10 -0300

    fix: data ecr public

--- modules/api-gateway-configuration/data.tf
+++ modules/api-gateway-configuration/data.tf
@@ -1,8 +1,8 @@
 data "aws_default_tags" "tags" {}
 data "aws_caller_identity" "current" {}
-data "aws_ecrpublic_authorization_token" "token" {
-  provider = aws.virginia
-}
+# data "aws_ecrpublic_authorization_token" "token" {
+#   provider = aws.virginia
+# }
 data "external" "nlb_dns_name" {
   program = ["bash", "${path.module}/get_nlb_dns.sh"]
 }
