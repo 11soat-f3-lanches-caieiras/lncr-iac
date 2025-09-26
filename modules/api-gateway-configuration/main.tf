af6c54a8d9fd58bd05d308c4e89506df0aef547e
titoparizotto <titoparizotto@gmail.com>
2025-09-26 18:56:26 -0300

    fix: Correção do lambda_function_arn

--- modules/api-gateway-configuration/main.tf
+++ modules/api-gateway-configuration/main.tf
@@ -17,11 +17,7 @@ resource "aws_apigatewayv2_integration" "eks_nlb" {
   connection_id          = aws_apigatewayv2_vpc_link.eks_vpc_link.id
 }
 
-resource "aws_apigatewayv2_route" "lambda_root_route" {
-  api_id    = var.api_gateway_api_id
-  route_key = "ANY /"
-  target    = "integrations/${aws_apigatewayv2_integration.eks_nlb.id}"
-}
+
 
 #========================================================================================#
 #                                API CUSTOM AUTHORIZER                                   #
@@ -37,11 +33,10 @@ resource "aws_apigatewayv2_authorizer" "lambda_integration" {
   enable_simple_responses = true
 }
 
-
 resource "aws_apigatewayv2_route" "secured_route" {
   for_each = toset(var.authorization_routes)
   api_id    = var.api_gateway_api_id
-  route_key = replace(each.value, "//$", "/")
+  route_key = each.value
   authorization_type = "CUSTOM"
   authorizer_id      = aws_apigatewayv2_authorizer.lambda_integration.id
   target             = "integrations/${aws_apigatewayv2_integration.eks_nlb.id}"
@@ -50,6 +45,12 @@ resource "aws_apigatewayv2_route" "secured_route" {
     ignore_changes = [route_key]
   }
 }
+
+# resource "aws_apigatewayv2_route" "lambda_root_route" {
+#   api_id    = var.api_gateway_api_id
+#   route_key = "ANY /"
+#   target    = "integrations/${aws_apigatewayv2_integration.eks_nlb.id}"
+# }
 
 
 
