e4edd10b15899b6d21fd038a4a2d2985f197c096
gusta_silva <gustavosilva18052005@gmail.com>
2025-09-27 07:49:36 -0300

    fix: variable call and remove deploy workflow

--- variables.tf
+++ variables.tf
@@ -88,6 +88,7 @@ variable "api_gateway_throttle" {
 variable "eks_nlb_listener_arn" {
   description = "arn of the EKS NLB listener for API Gateway integration"
   type        = string
+  default = ""
 }
 
 
