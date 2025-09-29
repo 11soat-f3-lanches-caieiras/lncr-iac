6a1c1d04c32900d9c6a24be705d24cac4e214d82
titoparizotto <titoparizotto@gmail.com>
2025-09-29 09:37:28 -0300

    devops: corrigindo lambda e adicionando policie de ElasticLoadBalancingFullAccess ao alb

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -9,7 +9,7 @@ data "archive_file" "lambda_zip" {
 }
 
 data "aws_lambda_function" "existing" {
-  function_name = var.function_name
+  function_name = "${var.prefix_name}-${var.environment_name}-${var.function_name}"
 
   lifecycle {
     postcondition {
