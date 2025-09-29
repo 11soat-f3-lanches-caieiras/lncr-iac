148aab8419bc3d42d5a70fe8b4012d50ca47d844
titoparizotto <titoparizotto@gmail.com>
2025-09-29 09:32:41 -0300

    devops: corrigindo lambda e adicionando policie de ElasticLoadBalancingFullAccess ao alb

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,14 +1,15 @@
 output "lambda_function_arn" {
   description = "Lambda function ARN"
-  value       = aws_lambda_function.lambda.arn
+  value       = length(aws_lambda_function.lambda) > 0 ? aws_lambda_function.lambda[0].arn : data.aws_lambda_function.existing.arn
 }
 
 output "lambda_function_name" {
   description = "Lambda function name"
-  value       = aws_lambda_function.lambda.function_name
+  value       = length(aws_lambda_function.lambda) > 0 ? aws_lambda_function.lambda[0].function_name : data.aws_lambda_function.existing.function_name
 }
 
 output "lambda_invoke_arn" {
   description = "Lambda function invoke ARN"
   value       = aws_lambda_function.lambda.invoke_arn
 }
+
